1a999d6c09c8b71ef62fadf7e3962c23513ff678
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-07-05 22:30:37 -0500

    Fix ubuntu1604.clang.ossfuzz

---
+++ scripts/docker/buildpack/test/test-ubuntu1604.clang.ossfuzz.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /tmp
+mkdir fuzzer-build && cd fuzzer-build
+
+## Compile protobuf C++ bindings
+protoc --proto_path=../test/tools/ossfuzz yulProto.proto --cpp_out=../test/tools/ossfuzz
+protoc --proto_path=../test/tools/ossfuzz abiV2Proto.proto --cpp_out=../test/tools/ossfuzz
+protoc --proto_path=../test/tools/ossfuzz solProto.proto --cpp_out=../test/tools/ossfuzz
+
+## Run cmake
+cmake -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/libfuzzer.cmake -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" ..
+make ossfuzz ossfuzz_proto ossfuzz_abiv2
