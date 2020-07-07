d9eca8c2d6f254c21749ed7287dfc87b428906c5
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-07-07 17:14:36 -0500

    Test review comment on PR.

--- .github/actions/create-issue/src/index.ts
+++ .github/actions/create-issue/src/index.ts
@@ -7,32 +7,38 @@ async function run() {
   const octokit = new github.GitHub(token);
   const context = github.context;
 
-  const newIssue = await octokit.issues.create({
-    ...context.repo,
-    labels: [`buildpack-review`],
-    title: `${core.getInput("package-name")} ${core.getInput("package-version")} ready for review`,
-    body: `# :rocket: ${core.getInput("package-name")}  ${core.getInput("package-version")} ready for review`
-  });
+  // const newIssue = await octokit.issues.create({
+  //   ...context.repo,
+  //   labels: [`buildpack-review`],
+  //   title: `${core.getInput("package-name")} ${core.getInput("package-version")} ready for review`,
+  //   body: `# :rocket: ${core.getInput("package-name")}  ${core.getInput("package-version")} ready for review`
+  // });
+
+  // await octokit.issues.createComment({
+  //   ...context.repo,
+  //   issue_number: newIssue.data.number,
+  //   body: `Action: ${core.getInput("action-id")}`
+  // });
 
   await octokit.issues.createComment({
     ...context.repo,
-    issue_number: newIssue.data.number,
-    body: `Action: ${core.getInput("action-id")}`
-  });
-
-  await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: newIssue.data.number,
+    issue_number: context.payload.pull_request!.number,
     body: `Artifact: ${core.getInput("artifact")}`
   });
 
-  await octokit.issues.createComment({
-    ...context.repo,
-    issue_number: newIssue.data.number,
-    body: `Commit: ${context.sha}`
-  });
-
-  core.setOutput("issue-id", newIssue.data.number.toString());
+  // await octokit.issues.createComment({
+  //   ...context.repo,
+  //   issue_number: newIssue.data.number,
+  //   body: `Artifact: ${core.getInput("artifact")}`
+  // });
+  //
+  // await octokit.issues.createComment({
+  //   ...context.repo,
+  //   issue_number: newIssue.data.number,
+  //   body: `Commit: ${context.sha}`
+  // });
+  //
+  // core.setOutput("issue-id", newIssue.data.number.toString());
 }
 
 run();
