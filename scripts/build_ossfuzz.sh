e5bb7636cbd7ac05a988e7923e4a16297444caf4
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-07-09 10:52:19 -0500

    Refactoring.

--- scripts/build_ossfuzz.sh
+++ scripts/build_ossfuzz.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-cd /tmp
-mkdir fuzzer-build && cd fuzzer-build
+ROOTDIR="$(dirname "$0")/.."
+BUILDDIR="${ROOTDIR}/build"
 
-## Compile protobuf C++ bindings
+mkdir -p "${BUILDDIR}"
+cd "${BUILDDIR}"
+
 protoc --proto_path=../test/tools/ossfuzz yulProto.proto --cpp_out=../test/tools/ossfuzz
 protoc --proto_path=../test/tools/ossfuzz abiV2Proto.proto --cpp_out=../test/tools/ossfuzz
 protoc --proto_path=../test/tools/ossfuzz solProto.proto --cpp_out=../test/tools/ossfuzz
-
-## Run cmake
-cmake -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/libfuzzer.cmake -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" ..
-make ossfuzz ossfuzz_proto ossfuzz_abiv2
+cmake .. -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:-Release} $CMAKE_OPTIONS
+make ossfuzz ossfuzz_proto ossfuzz_abiv2 -j4
