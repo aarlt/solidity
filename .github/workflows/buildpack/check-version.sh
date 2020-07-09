15100b74bdbc90a98bebc253012ec9520c62891a
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-07-08 19:17:28 -0500

    check-version.sh: corrected for emscripten

--- .github/workflows/buildpack/check-version.sh
+++ .github/workflows/buildpack/check-version.sh
@@ -9,13 +9,19 @@ fi
 VERSION=$(grep -e 'LABEL version=\".*\"' "scripts/docker/${IMAGE_NAME}/Dockerfile.${IMAGE_VARIANT}" | awk -F"\"" '{ print $2 }')
 echo "::set-env name=VERSION::${VERSION}"
 
+DOCKER_IMAGE_ID="$IMAGE_NAME-$IMAGE_VARIANT"
+if [ "${IMAGE_VARIANT}" == "emscripten" ]; then
+  EMSDK_VERSION=$(grep "emscripten/emsdk" scripts/docker/buildpack/Dockerfile.emscripten | cut -f 2 -d ' ' | cut -f 2 -d ':')
+  DOCKER_IMAGE_ID="${DOCKER_IMAGE_ID}-${EMSDK_VERSION}"
+fi
+
 echo "${GITHUB_TOKEN}" | docker login docker.pkg.github.com -u "${GITHUB_ACTOR}" --password-stdin
-VERSION_EXIST=$(docker pull -q "${GITHUB_DOCKER_REPOSITORY}/${IMAGE_NAME}-${IMAGE_VARIANT}:${VERSION}" || echo false && true)
+VERSION_EXIST=$(docker pull -q "${GITHUB_DOCKER_REPOSITORY}/${DOCKER_IMAGE_ID}:${VERSION}" || echo false && true)
 docker logout docker.pkg.github.com
 
 if [ "${VERSION_EXIST}" != "false" ]; then
   echo ""
-  echo "ERROR: version '${VERSION}' already exist for '${IMAGE_NAME}-${IMAGE_VARIANT}'. Aborting."
+  echo "ERROR: version '${VERSION}' already exist for '${DOCKER_IMAGE_ID}'. Aborting."
   echo ""
 
   false
