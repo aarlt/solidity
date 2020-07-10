960cad17a0402f18b8b5c334eb12ceb73b836d83
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-07-09 20:39:45 -0500

    iii

--- scripts/build_ossfuzz.sh
+++ scripts/build_ossfuzz.sh
@@ -15,5 +15,5 @@ cd "${BUILDDIR}"
 protoc --proto_path=../test/tools/ossfuzz yulProto.proto --cpp_out=../test/tools/ossfuzz
 protoc --proto_path=../test/tools/ossfuzz abiV2Proto.proto --cpp_out=../test/tools/ossfuzz
 protoc --proto_path=../test/tools/ossfuzz solProto.proto --cpp_out=../test/tools/ossfuzz
-cmake .. -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/libfuzzer.cmake -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" "$CMAKE_OPTIONS"
+cmake .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/libfuzzer.cmake
 make ossfuzz ossfuzz_proto ossfuzz_abiv2 -j4
