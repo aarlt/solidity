92974a61357e6c971e0447c3bf085a556104b7e3
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-07-09 19:54:45 -0500

    iiii

--- scripts/build_ossfuzz.sh
+++ scripts/build_ossfuzz.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 set -e
 
-ROOTDIR="/root/project/"
-BUILDDIR="${ROOTDIR}/build"
-
-if test ! -z "$1"; then
-	BUILDDIR="$1"
+ROOTDIR="$(dirname "$0")/.."
+if [[ "$( cd "$(dirname "$0")" ; pwd -P )" == *".github"* ]]
+then
+    ROOTDIR="/root/project"
 fi
 
-echo ">>>> ${BUILDDIR}"
+BUILDDIR="${ROOTDIR}/build"
 
 mkdir -p "${BUILDDIR}"
 cd "${BUILDDIR}"
 
-echo ">>>> ${PWD}"
-
-ls ../
-
-echo "-----------"
-
 protoc --proto_path=../test/tools/ossfuzz yulProto.proto --cpp_out=../test/tools/ossfuzz
 protoc --proto_path=../test/tools/ossfuzz abiV2Proto.proto --cpp_out=../test/tools/ossfuzz
 protoc --proto_path=../test/tools/ossfuzz solProto.proto --cpp_out=../test/tools/ossfuzz
-cmake .. -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/libfuzzer.cmake
+cmake .. -DCMAKE_TOOLCHAIN_FILE=cmake/toolchains/libfuzzer.cmake -DCMAKE_BUILD_TYPE="${CMAKE_BUILD_TYPE:-Release}" "$CMAKE_OPTIONS"
 make ossfuzz ossfuzz_proto ossfuzz_abiv2 -j4
